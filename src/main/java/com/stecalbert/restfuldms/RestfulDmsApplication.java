4302ac4ff04679da388c1df61c2f404d2c82b1d5
albert.stec <albert.stec@asseco.pl>
2019-03-25 14:59:36 +0100

    Initial commit

---
+++ src/main/java/com/stecalbert/restfuldms/RestfulDmsApplication.java
@@ -0,0 +1,13 @@
+package com.stecalbert.restfuldms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulDmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulDmsApplication.class, args);
+    }
+
+}

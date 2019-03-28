4d066b0264f708b4bf5886e1f52884e06f0bb0e6
albert.stec <albert.stec@gmail.com>
2019-03-28 18:34:35 +0100

    Finish unit tests for current user routes

--- src/test/java/com/stecalbert/restfuldms/controller/UserControllerTest.java
+++ src/test/java/com/stecalbert/restfuldms/controller/UserControllerTest.java
@@ -28,10 +28,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 public class UserControllerTest {
-    private static final String TOKEN =
+    private static final String DOMAIN_TOKEN =
             "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9" +
                     ".eyJzdWIiOiJlbG8iLCJleHAiOjE1NTQ0MDk4MDZ9" +
                     ".exgHQqyAjMeRmbHT8ii2eE1iaLLKtnHaQxDkXJdBDLSCaWqx1Qy314SvPUGAKm7IT1hcLuydOso9JtEjOWUnLw";
+
+    private static final String ADMIN_TOKEN = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9" +
+            ".eyJzdWIiOiJzdXBlci11c2VyIiwiZXhwIjoxNTU0NjU3MzcxLCJhdXRob3JpdGllcyI6WyJBRE1JTiIsIkRPTUFJTiIsIk1PREVSQVRPUiJdfQ" +
+            ".B_obMAWv8rmeMXqO8KfFOOif40Btgq5hvr5EACeXiuXuezC_WB8TErJbpMELYfuxF_3586P_lgJGzN7qh0uxJQ";
 
     @MockBean
     private UserService userService;
@@ -56,13 +60,26 @@ public class UserControllerTest {
         Mockito.when(userService.findAll()).thenReturn(getMockUserEntityList());
         RequestBuilder requestBuilder = MockMvcRequestBuilders
                 .get("/users")
-                .header("Authorization", TOKEN)
+                .header("Authorization", ADMIN_TOKEN)
                 .contentType(MediaType.APPLICATION_JSON);
 
         mvc.perform(requestBuilder)
                 .andExpect(status()
                         .isOk())
                 .andExpect(jsonPath("$", hasSize(3)));
+    }
+
+    @Test
+    public void givenValidTokenIncorrectRole_whenGetAllUsers_thenForbidden() throws Exception {
+        Mockito.when(userService.findAll()).thenReturn(getMockUserEntityList());
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .get("/users")
+                .header("Authorization", DOMAIN_TOKEN)
+                .contentType(MediaType.APPLICATION_JSON);
+
+        mvc.perform(requestBuilder)
+                .andExpect(status()
+                        .isForbidden());
     }
 
 

1654733bdc7986fed599b60c500dc266fcaf5a1f
albert-stec <stecalbert@gmail.com>
2019-03-30 18:18:58 +0100

    Login screen intro section with unfinished login card

--- restful-dms-ui/src/app/app.module.ts
+++ restful-dms-ui/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,

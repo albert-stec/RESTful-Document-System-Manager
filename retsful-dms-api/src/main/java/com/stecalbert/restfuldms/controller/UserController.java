03b4563fc7ceb3911b8fbe5d1570b0a6919521e4
albert-stec <stecalbert@gmail.com>
2019-04-09 23:28:58 +0200

    Add controller description to swagger ui Add authorization locks to swagger ui Set readOnly properties in swagger ui in order to hide them from example requests

--- retsful-dms-api/src/main/java/com/stecalbert/restfuldms/controller/UserController.java
+++ retsful-dms-api/src/main/java/com/stecalbert/restfuldms/controller/UserController.java
@@ -3,10 +3,13 @@ package com.stecalbert.restfuldms.controller;
 import com.stecalbert.restfuldms.model.dto.UserDto;
 import com.stecalbert.restfuldms.model.entity.UserEntity;
 import com.stecalbert.restfuldms.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.Authorization;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +19,15 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 import java.util.List;
 
+@Api(value = "US", description = "<div>\n" +
+        "<p>The User Service (US) offers the following services</p><ul>\n" +
+        "<li>Register new user;</li>\n" +
+        "<li>Get all users or get all users for given role (parametrized);</li>\n" +
+        "<li>Get single user by id;</li>\n" +
+        "<li>Delete user;</li>\n" +
+        "<li>Disable/Enable user;</li>\n" +
+        "<li>Update user;</li>\n" +
+        "</ul></div>", tags = "User Service (US)")
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -37,7 +49,9 @@ public class UserController {
     }
 
     @GetMapping
-    public ResponseEntity<List<UserDto>> getAll(Authentication authentication) {
+    @ApiOperation(value = "Get all users",
+            authorizations = @Authorization(HttpHeaders.AUTHORIZATION))
+    public ResponseEntity<List<UserDto>> getAll() {
 
         return new ResponseEntity<>(
                 userService.findAll(),
--- retsful-dms-api/src/main/java/com/stecalbert/restfuldms/model/dto/UserDto.java
+++ retsful-dms-api/src/main/java/com/stecalbert/restfuldms/model/dto/UserDto.java
@@ -1,6 +1,7 @@
 package com.stecalbert.restfuldms.model.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,6 +16,7 @@ import java.util.Set;
 @AllArgsConstructor
 public class UserDto {
 
+    @ApiModelProperty(readOnly = true)
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Long id;
 
@@ -28,6 +30,7 @@ public class UserDto {
 
     private @NotBlank String email;
 
+    @ApiModelProperty(readOnly = true)
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Set<AuthorityDto> authorities;
 }

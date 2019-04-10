d17bf63e113736001a56aa05a2e6c3ed5bdb9d29
albert-stec <stecalbert@gmail.com>
2019-04-10 12:45:18 +0200

    Add fortawesome icons

--- restful-dms-ui/src/app/app.module.ts
+++ restful-dms-ui/src/app/app.module.ts
@@ -2,7 +2,7 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule, NO_ERRORS_SCHEMA} from '@angular/core';
 import {ReactiveFormsModule} from '@angular/forms';
 import {AppComponent} from './app.component';
-import {MDBBootstrapModule} from 'angular-bootstrap-md';
+import {IconsModule, MDBBootstrapModule} from 'angular-bootstrap-md';
 import {LoginComponent} from './login/login.component';
 import {routing} from './app-routing.module';
 import {HomeComponent} from './home/home.component';
@@ -21,6 +21,7 @@ import {NavigationComponent} from './navigation/navigation.component';
     ReactiveFormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
+    IconsModule,
     ToastrModule.forRoot({
       timeOut: 5000,
       positionClass: 'toast-top-center',

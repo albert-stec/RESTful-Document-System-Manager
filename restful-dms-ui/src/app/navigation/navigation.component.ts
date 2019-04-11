1a4581b213fa9225aea0710b7b38588fa03153bd
albert-stec <stecalbert@gmail.com>
2019-04-11 18:40:33 +0200

    Move profile from sidebar to navbar

--- restful-dms-ui/src/app/navigation/navigation.component.ts
+++ restful-dms-ui/src/app/navigation/navigation.component.ts
@@ -1,4 +1,7 @@
 import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
+import {AuthenticationService} from "../services/authentication.service";
+import {User} from "../models/user";
 
 @Component({
   selector: 'app-navigation',
@@ -6,15 +9,21 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./navigation.component.scss']
 })
 export class NavigationComponent implements OnInit {
+  currentUser: User;
 
-  constructor() {
+  constructor(
+    private router: Router,
+    private authenticationService: AuthenticationService
+  ) {
+    this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
   }
 
   ngOnInit() {
   }
 
   logout() {
-    alert("wylogowano");
+    this.authenticationService.logout();
+    this.router.navigate(['/login']);
   }
 
 }
